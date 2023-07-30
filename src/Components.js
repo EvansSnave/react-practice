3d5d685acbbd0c2760944de4ce422356bf948876
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-07-30 16:36:36 -0500

    dinamic style

--- src/Components.js
+++ src/Components.js
@@ -4,10 +4,18 @@ const Changes = () => {
   const [red, setRed] = useState(true);
   const color = red?'red':'blue';
   const contrary = red?'blue':'red';
+  const styleH1 = {
+    backgroundColor: `${color}`,
+    color: `${red?'black':'white'}`,
+  }
+  const styleButton = {
+    backgroundColor: `${contrary}`,
+    color: `${contrary === 'blue'?'white': 'gray'}`,
+  }
   return (
     <div>
-      <h1>My color is {color}</h1>
-      <button type="button" onClick={() => setRed(!red)}>Change to {contrary}</button>
+      <h1 style={styleH1}>My color is {color}</h1>
+      <button style={styleButton} type="button" onClick={() => setRed(!red)}>Change to {contrary}</button>
     </div>
   );
 }
