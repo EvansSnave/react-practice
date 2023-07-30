8673bb185d2ed6af5a6762f51515525a227d34bc
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-07-30 16:11:20 -0500

    proposed solution

--- src/Components.js
+++ src/Components.js
@@ -2,10 +2,12 @@ import { useState } from "react";
 
 const Changes = () => {
   const [red, setRed] = useState(true);
+  const color = red?'red':'blue';
+  const contrary = red?'blue':'red';
   return (
     <div>
-      <h1>My color is </h1>
-      <button type="button" onClick={() => setRed(!red)}>Change to {red?'red':'blue'}</button>
+      <h1>My color is {color}</h1>
+      <button type="button" onClick={() => setRed(!red)}>Change to {contrary}</button>
     </div>
   );
 }
