fa54d3bea4910016409d178d1168711004a1d7aa
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-07-30 15:59:41 -0500

    prac

--- src/Components.js
+++ src/Components.js
@@ -1,28 +1,12 @@
-import { useEffect, useState } from "react";
+import useState from "react";
 
-const Changes = (props) => {
-  const [object, setObject] = useState({
-    name: props.name,
-    color: 'red',
-  });
-
-  useEffect(() => {
-    document.querySelector('.color').textContent = `My color is ${object.color}`;
-  }, [object.color]);
-
-  const update = (color) => {
-    setObject( previous => {
-      return { ...previous, color: color}
-    })
-  }
-
-return (
-  <div>
-    <p>My name is { object.name }</p>
-    <p className="color"></p>
-    <button className="buttonChange" type="button" onClick={object.color === 'red'?() => update('blue'): () => update('red')}>
-      Change to {object.color === 'red'? 'blue': 'red'}</button>
-  </div>
+const Changes = () => {
+  const [red, setRed] = useState(true);
+  return (
+    <div>
+      <h1>My color is </h1>
+      <button type="button" onClick={() => setRed(!red)}>Change to {red?'red':'blue'}</button>
+    </div>
   );
 }
 
